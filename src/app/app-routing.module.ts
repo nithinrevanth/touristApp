1fa9d2d19fbc4b389d1acbe60801ad350cba9555
nithin revanth m <nithin.revanth@gmail.com>
2023-08-05 18:14:54 +0530

    first few changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NopageComponent } from './nopage/nopage.component';
+import { LoginComponent } from './login/login.component';
+import { DataComponent } from './data/data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "data", component: DataComponent },
+  // Empty path
+  {path:"",redirectTo:"home",pathMatch:"full"},
+  // Invalidpath
+  {path:"**",component:NopageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {  HttpClientModule } from '@angular/common/http';
+import { NopageComponent } from './nopage/nopage.component';
+import { LoginComponent } from './login/login.component';
+import { DataComponent } from './data/data.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageComponent,
+    LoginComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
